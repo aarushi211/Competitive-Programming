3ec01a51fa91b6d06f80878961049baf90fd9750
aarushi211 <aarushi.jain211@gmail.com>
2022-11-23 20:36:13 +0530

    added new function

--- Leetcode/MergeList.java
+++ Leetcode/MergeList.java
@@ -77,4 +77,20 @@ class SortedList{
         }
         return temp.next;
     }
+    
+    Node sortedMerge1(Node list1, Node list2){
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        if(list1.data<= list2.data){
+            list1.next = sortedMerge1(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = sortedMerge1(list1, list2.next);
+            return list2;
+        }
+    }
 }
